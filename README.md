2917232c4111033e69d4a336241b4c74d51a1e03
Marcos <macarrfer@gmail.com>
2016-11-26 15:27:45 +0100

    Finished first implementation to match given exampleResult. Next steps will be focused in optimization

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Travelling-Salesman
 This is a python implementation with aim to solve the Travelling salesman problem
+
+## Tabu-search branch 
+Insted of using a best-first criteria for selecting the best path, this implementation is going to use a **Tabu-search** aproach
--- shortPathFinder.py
+++ shortPathFinder.py
@@ -2,21 +2,38 @@ import random
 import math
 import fileManager
 
+
 class TravellingSalesman:
     #Properties of the class
+    #Matrix
+    distanceMatrix=[]
+    actualSolution=[]
+    solution=[]
+    minimalDistance=0
+    tabuList=[]
+    neighbors=[]
+    bestNeighbor=[]
+    #Integers
+    iterations=0
+    solIteration=0
+    bestIndex0=0
+    bestIndex1=0
+    iterationsWithoutImprovement=0
+    maxIterationsWithoutImprovement=100
+    improvement=0
+    restart=0
+
     numberOfCities=0
     solutionNumber = -1
-    neighborNumber=-1
-    distanceMatrix=[]
+    neighborNumber=0
     pathToRandomFile=0
-    actualSolution=[]
     actualDistance=0
     newDistance=0
-    neighbors = []
-    swapMatrix=[]
-    swapArrayCount=0
-    swapArrayDimension=0
+
+    maxIterationCount=0
+    maxIterations=10000
     randomFile = 0
+    tendencyParameter=100
 
     #Intial function
     def __init__(self,pathToDistances,pathToRandomFile=0):
@@ -26,16 +43,13 @@ class TravellingSalesman:
         self.distanceMatrix = fileManager.openFile(pathToDistances, self.numberOfCities)
         # #The next step for this project is to obtain a neighbor matrix that we're going to use not to generate
         # #two times the same neighbor
-        self.swapMatrix=[[0] * (i+1) for i in range(self.numberOfCities)]
-        for i in range(0,self.numberOfCities):
-            self.swapMatrix[i][i]=1
+        self.tendencyParameter=self.numberOfCities
         self.numberOfCities-=1
         if(pathToRandomFile)!=0:
             self.pathToRandomFile=pathToRandomFile
             self.randomFile=open(pathToRandomFile,"r")
         #We determine the maximum of swiches that we can do
         self.swapArrayDimension=sum(range(0,self.numberOfCities, 1))
-
 
     #Print different elements
     def printDistanceMatrix(self):
@@ -44,7 +58,7 @@ class TravellingSalesman:
             print(line)
 
     def printActualSolution(self):
-        print("SOLUCION S_", self.solutionNumber, " -> ", self.actualSolution, "; ",self.actualDistance,"km",sep='')
+        print("\tRECORRIDO:", *self.actualSolution,"")
 
     def printSwapMatrix(self):
         print("Swaps done matrix: ")
@@ -69,23 +83,6 @@ class TravellingSalesman:
             value = float(self.randomFile.readline())
             return value
 
-    def calculateInitialDistance(self):
-        i = 0
-        index01 = 0
-        index02 = 0
-        self.actualDistance = self.distanceMatrix[self.actualSolution[0] - 1][0]
-        while i < (self.numberOfCities - 1):
-            index01 = self.actualSolution[i]
-            index02 = self.actualSolution[i + 1]
-            if index01 > index02:
-                self.actualDistance += int(self.distanceMatrix[index01 - 1][index02])
-            else:
-                self.actualDistance += int(self.distanceMatrix[index02 - 1][index01])
-
-            i += 1
-        self.actualDistance += self.distanceMatrix[self.actualSolution[self.numberOfCities - 1]-1][0]
-        self.newDistance=self.actualDistance
-
     def calculateDistance(self,citiesVector):
         i = 0
         index01 = 0
@@ -101,6 +98,9 @@ class TravellingSalesman:
 
             i += 1
         self.newDistance += self.distanceMatrix[citiesVector[self.numberOfCities - 1]-1][0]
+        if self.actualDistance == 0:
+            self.actualDistance=self.newDistance
+        return self.newDistance
 
     #generates an initial vector with random values
     def generateInitialSolution(self):
@@ -117,67 +117,94 @@ class TravellingSalesman:
             index+=1
         #if path has a value, then we read the random numbers from the specified file
         self.solutionNumber+=1
-        self.calculateInitialDistance()
-
+        self.calculateDistance(self.actualSolution)
+        self.minimalDistance=self.actualDistance
     #
     def swap(self,vector,index0,index1):
         returnVector = list(vector)
         returnVector[index0] = vector[index1]
         returnVector[index1] = vector[index0]
-        self.swapArrayCount+=1
+        self.maxIterationCount+=1
         return returnVector
-    #
+    # Calculates all neighbors and checks who is teh best solution
     def calculateNeighbors(self):
-        #self.neighbors=[[0]*self.numberOfCities]
-        # self.neighbors=[self.actualSolution]
-        self.swapArrayCount=0
-        vector=self.actualSolution
-        i=-1
-        lineCount=0
-        #We repeat the bucle while we haven't reached a better solution
-        print()
-        self.printActualSolution()
-        while self.newDistance >= self.actualDistance and self.swapArrayCount < self.swapArrayDimension:
-            self.neighbors.append(vector)
-            self.neighborNumber+=1
-            lineCount=0
-            # First we've got to choose two random numbers to swap them
-            #element0=math.floor(self.calculateRandom() * self.numberOfCities)
-            # element0=1+math.floor(self.calculateRandom() *self.numberOfCities)
+        line=0
+        tabuCount=0
+        while line < self.maxIterations:
+            index0=0
+            index1=0
+            line+=1
+            jump=0
+            first=0
+            #calculates 1 iteration
 
-            element0 = math.floor(self.calculateRandom() * self.numberOfCities)
+            while index0<=99 and index1<98:
+                if jump==index1:
+                    index0+=1
+                    index1=0
+                    jump+=1
+                # here goes the actual content
+                vector=self.swap(self.actualSolution,index0,index1)
+                if [index0, index1] not in self.tabuList:
+                    if ((self.calculateDistance(vector) < self.actualDistance) or first==0):
+                        first=1
+                        self.actualDistance=self.newDistance
+                        self.bestNeighbor=vector
+                        self.bestIndex0=index0
+                        self.bestIndex1=index1
+                index1+=1
 
-            element1 = math.floor(self.calculateRandom() * self.numberOfCities)
+            self.iterations+=1
+            self.actualSolution=self.bestNeighbor
 
-            # print("element0 - element1",element0,element1)
+            if [self.bestIndex0,self.bestIndex1] not in self.tabuList:
+                self.tabuList.append([self.bestIndex0, self.bestIndex1])
+            tabuCount += 1
 
-            while self.swapMatrix[max(element0,element1)][min(element0,element1)]==1:
-                if element0 == element1:
-                    element0 +=1
-                    element0 = math.fmod(element0,self.numberOfCities)
-                    element0 = int(element0)
-                    element1 = 0
-                else:
-                    tmp=max(element0,element1)
-                    element1=min(element0,element1)
-                    element1+=1
-                    element1 = math.fmod(element1, self.numberOfCities)
-                    element1=int(element1)
-                    element0=tmp
-                    # print("element0 - element1", element0, element1)
-            vector=self.swap(self.actualSolution,max(element0,element1),min(element0,element1))
-            self.calculateDistance(vector)
-            # print("element0 - element1",element0,element1)
-            self.swapMatrix[max(element0,element1)][min(element0,element1)]=1
-            print("\tVECINO V_", self.neighborNumber, " -> Intercambio: (",max(element0,element1),", ",min(element0,element1),"); ", vector, "; ", self.newDistance,"km",sep='')
+            if self.actualDistance < self.minimalDistance:
+                self.solution=self.bestNeighbor
+                self.solIteration=self.iterations
+                self.minimalDistance=self.actualDistance
+                self.iterationsWithoutImprovement=0
+            else:
+                self.iterationsWithoutImprovement+=1
 
-        self.actualSolution=vector
-        self.neighbors=[]
-        self.reinitializeSwapMatrix()
-        self.actualDistance=self.newDistance
-        self.solutionNumber+=1
-        self.neighborNumber=-1
+            print("ITERACION:", self.iterations)
+            print("\tINTERCAMBIO: (", self.bestIndex0, ", ", self.bestIndex1, ")", sep='')
+            self.printActualSolution()
+            print("\tCOSTE (km):", self.actualDistance)
+            print("\tITERACIONES SIN MEJORA:", self.iterationsWithoutImprovement)
+            print("\tLISTA TABU:")
+            if tabuCount > self.tendencyParameter:
+                self.tabuList.remove(self.tabuList[0])
+                tabuCount -= 1
+            for element in self.tabuList:
+                print("\t", element[0], " ", element[1], sep='')
+
+
+            if self.iterationsWithoutImprovement == self.maxIterationsWithoutImprovement:
+                print()
+                self.restart+=1
+                print("***************")
+                print("REINICIO:",self.restart)
+                print("***************")
+                self.tabuList.clear()
+                self.iterationsWithoutImprovement=0
+                self.actualSolution=self.solution
+                self.actualDistance=self.minimalDistance
+                tabuCount=0
+            print()
+
     #
     def run(self):
-        while self.swapArrayCount < self.swapArrayDimension:
+            print("RECORRIDO INICIAL")
+            self.printActualSolution()
+            print("\tCOSTE (km):",self.newDistance)
+            print()
             self.calculateNeighbors()
+            print()
+            print("MEJOR SOLUCION: ")
+            self.actualSolution=self.solution
+            self.printActualSolution()
+            print("\tCOSTE (km):", self.minimalDistance)
+            print("\tITERACION:",self.solIteration)
